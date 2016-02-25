48e283842d34e5b1d52810c59f8236ca0af22d98
Bharath <GEngines@users.noreply.github.com>
2016-02-26 00:51:31 +0530

    Update updateFireFoxSettings.bat

--- updateFireFoxSettings.bat
+++ updateFireFoxSettings.bat
@@ -4,6 +4,6 @@ REM --  Load up the Python Library
 
 @echo off
 
-c:\python27\python.exe "D:\Design\Scripts and Shaders\PythonScript\test2.py"
+"c:\python27\python.exe" "D:\Design\Scripts and Shaders\PythonScript\test2.py" "proxyList"
 
 pause
